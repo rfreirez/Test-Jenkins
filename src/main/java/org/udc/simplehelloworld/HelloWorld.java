5efb67d4ca109087c9079dd46716c5e157bc18a0
rfreirez <renangfz@hotmail.com>
2017-06-08 11:26:50 -0500

    Create HelloWorld.java

--- src/main/java/org/udc/simplehelloworld/HelloWorld.java
+++ src/main/java/org/udc/simplehelloworld/HelloWorld.java
@@ -13,7 +13,7 @@ public class HelloWorld {
     
 
     public static void main(String[] args) {
-        System.out.println("Hello World ..."+name);
+        System.out.println("Hello World ...");
     }
 
 }
